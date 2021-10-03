9335dcf36c34ce34f4f7154c0780066ed33a3fb7
RSimer <65471088+RSimer@users.noreply.github.com>
2021-10-02 21:24:02 -0700

    added in resume

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -28,7 +28,7 @@ export function Sider() {
       <Menu.Item key="1"> <Link to = "/about"> About Me </Link> </Menu.Item>
       <Menu.Item key="2"> <Link to = "/projectList"> Projects </Link> </Menu.Item>
       <Menu.Item key="3"> <Link to = '/contact'> Contact Me </Link> </Menu.Item>
-      <Menu.Item key="4">Resume</Menu.Item>
+      <Menu.Item key="4"> <a href='https://drive.google.com/file/d/1otn9nlF6xx614gdStzDz2a-71uI976GS/view?usp=sharing'> Resume  </a> </Menu.Item>
     </SubMenu>
   </Menu>
   );
